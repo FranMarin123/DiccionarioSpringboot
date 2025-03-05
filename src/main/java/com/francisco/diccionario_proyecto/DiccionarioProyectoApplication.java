5cbf1b0bf1e873362f27d6a0b1d0956bf37cbd24
Francisco <franciscomarinalvarez@hotmail.es>
2025-03-05 11:13:58 +0100

    first commit

---
+++ src/main/java/com/francisco/diccionario_proyecto/DiccionarioProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.diccionario_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiccionarioProyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiccionarioProyectoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/francisco/diccionario_proyecto/DiccionarioProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.diccionario_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiccionarioProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
